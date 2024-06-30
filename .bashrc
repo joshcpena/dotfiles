7e644754cc5baaebd011cd6de6b33180e0f23166
jp133020 <jpena@bloombergindustry.com>
2024-06-30 18:42:40 +0000

    reset

--- .bashrc
+++
@@ -1,4 +0,0 @@
-export PATH="$PATH:/opt/nvim-linux64/bin"
-
-export PATH="$PATH:$HOME/.local/bin"
-export PATH="$PATH:~/.cargo/env"
--- .config/nvim/lua/plugins/better-escape.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "https://github.com/max397574/better-escape.nvim",
-  config = function()
-    require("better_escape")
-  end,
-}
