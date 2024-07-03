a65fd3cbbcfbaf30cf35edcb8a8ec5f6dfc970db
jp133020 <jpena@bloombergindustry.com>
2024-07-03 09:37:01 -0400

    Added .wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,11 +4,33 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- Color config:
+config.color_scheme = "Catppuccin Macchiato"
+config.colors = {
+	background = "#27243f",
+}
+-- Fonts:
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.font_size = 12.8
+config.line_height = 1.05
+config.font_rules = {
+	{
+		intensity = "Bold",
+		font = wezterm.font({
+			family = "JetBrainsMono Nerd Font",
+			weight = "Bold",
+		}),
+	},
+}
+-- Misc:
+config.hide_tab_bar_if_only_one_tab = true
 
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 12.5
+config.window_padding = {
+	left = "0px",
+	right = "0px",
+	top = "0px",
+	bottom = "0px",
+}
+
 -- and finally, return the configuration to wezterm
 return config
