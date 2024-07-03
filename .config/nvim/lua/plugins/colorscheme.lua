6b0b12455c8a731ada35dee3e4f0f76e086b802d
jp133020 <jpena@bloombergindustry.com>
2024-07-03 13:30:25 +0000

    Fix bg

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,60 @@
 return {
   -- Configure LazyVim to load  catppuccin
-  { "EdenEast/nightfox.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = true,
+    config = function()
+      require("nightfox").setup({
+        specs = {
+          all = {
+            syntax = {
+              -- operator = "orange",
+              builtin0 = "pink.base",
+            },
+          },
+        },
+      })
+    end,
+  },
+  { "rebelot/kanagawa.nvim", lazy = true },
+  { "neanias/everforest-nvim", lazy = true },
+  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "bold" },
+        loops = { "bold" },
+        functions = {},
+        keywords = { "bold" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = { "bold" },
+        properties = {},
+        types = { "italic" },
+        operators = { "bold" },
+      },
+      color_overrides = {
+        macchiato = {
+          maroon = "#ee9bb6",
+          -- crust this is the dividor line between DBUI and the eidotr on the left
+          mantle = "#232136", -- the sidebar bg
+          base = "#27243f", -- The editor bg
+        },
+      },
+    },
+  },
+  { "talha-akram/noctis.nvim", lazy = true },
+  { "folke/tokyonight.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "duskfox",
     },
   },
 }
